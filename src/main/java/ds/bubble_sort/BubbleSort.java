513e987e28a474b3c3636448d670bc67bab95a30
ashish.adhikari@vodafone.com <ashish.adhikari@vodafone.com>
2018-07-09 11:08:09 +0000

    correction

--- src/main/java/ds/bubble_sort/BubbleSort.java
+++ src/main/java/ds/bubble_sort/BubbleSort.java
@@ -16,7 +16,7 @@ public class BubbleSort
 	static void bubbleSort(int arr[]) {
 		for(int i = 0; i < arr.length - 1 ; i++) {
 			for(int j = i+1; j<arr.length  ; j++) {
-				if(arr[j] < arr[i]) { // for ascending order . for decending order change to arr[j] < arr[i]
+				if(arr[j] < arr[i]) { // for ascending order . for decending order change to arr[j] > arr[i]
 					int x = arr[i];
 					arr[i] = arr[j];
 					arr[j] = x;
