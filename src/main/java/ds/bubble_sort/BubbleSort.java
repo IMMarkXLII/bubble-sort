26adad18a574b9390b30b29bcf6a5c0251e2c536
ashish.adhikari@vodafone.com <ashish.adhikari@vodafone.com>
2018-07-09 10:12:59 +0000

    bubble sort

---
+++ src/main/java/ds/bubble_sort/BubbleSort.java
@@ -0,0 +1,27 @@
+package ds.bubble_sort;
+
+import java.util.Arrays;
+
+public class BubbleSort
+{
+	public static void main( String[] args )
+	{
+		int arr[] = new int[] {18,16,90,-3};
+		
+		bubbleSort(arr);
+		
+		System.out.println(Arrays.toString(arr));
+		
+	}
+	static void bubbleSort(int arr[]) {
+		for(int i = 0; i < arr.length - 1 ; i++) {
+			for(int j = i+1; j<arr.length  ; j++) {
+				if(arr[j] < arr[i]) { // for ascending order . for decending order change to arr[j] < arr[i]
+					int x = arr[i];
+					arr[i] = arr[j];
+					arr[j] = x;
+				}
+			}
+		}
+	}
+}
